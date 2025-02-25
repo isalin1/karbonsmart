bfa3fe8effbb82081ce39599105f5a00fcc356ad
Ignacio Salinas <isalin1@hotmail.com>
2025-02-24 21:18:43 -0500

    confirmacion tablas movisaldos

--- src/movisaldoproveedores/dto/create-movisaldoproveedore.dto.ts
+++ src/movisaldoproveedores/dto/create-movisaldoproveedore.dto.ts
@@ -1,1 +1,11 @@
-export class CreateMovisaldoproveedoreDto {}
+import { IsInt } from "class-validator";
+
+export class CreateMovisaldoproveedoreDto {
+    @IsInt()
+    movimiento_id: number;
+    @IsInt()
+    negocioId: number;
+    @IsInt()
+    tipomovimientoId: number;
+    
+}
